26b14d5a11923a1376f91903707a35a6e10144cc
jackp50 <jpearne9@gmail.com>
2025-03-22 14:18:49 -0600

    Messing around with different loops, still have not figured out how to make a 3x4 grid while having them all be seperate

--- script.js
+++ script.js
@@ -40,7 +40,32 @@ function operate(num1, operator, num2) {
     }
 }
 
+function gridRows(rowAmount) {
+    for (let i = 0; i < rowAmount; i++ ) {
+        let grid = document.createElement("div");
+        grid.classList.add("row");
+        gridParent.appendChild(grid);
+
+    }
+}
+function gridColumn(columnAmount) {
+    for (let i = 0; i < columnAmount; i++ ) {
+        let grid = document.createElement("div");
+        grid.classList.add("column");
+        gridParent.appendChild(grid);
+    }
+}
+
+//Created my parent div "container" in the html file
 const container = document.querySelector("#container");
+//Div for the display screen
 const screenDiv = document.createElement("div");
 screenDiv.id = "screenDiv";
+screenDiv.textContent = "Display Screen";
 container.appendChild(screenDiv);
+//Parent container for the 4x4 grid, siblings with the screen div
+const gridParent = document.createElement("div");
+container.appendChild(gridParent);
+gridParent.id = "gridParent"
+gridRows(3);
+gridColumn(4);
