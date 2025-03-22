4374482c5948979316251c4fdd8415a6fa105e07
jackp50 <jpearne9@gmail.com>
2025-03-22 16:52:45 -0600

    I created a loop that goes through the array, gives each element an event listener with a click, then makes the screeDiv content equal to whatever was clicked. In order to make this second loop, I had to update my first loop (making the array). I had to start at 0 instead of 1... I should have just done this from the first place.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let num1 = null;
 let num2 = null;
 let operator = "";
 
+
 function add(num1, num2) {
     let result = num1 + num2;
     return result;
@@ -42,63 +43,76 @@ function operate(num1, operator, num2) {
 
 function buildGrid(squareAmount) {
     let finalGrid = [];
-    for (let i = 1; i <= squareAmount; i++ ) {
+    for (let i = 0; i < squareAmount; i++ ) {
         let grid = document.createElement("div");
         grid.classList.add("grid");
         gridParent.appendChild(grid);
         finalGrid[i] = grid;
     }
     console.log(finalGrid)
-    finalGrid[1].textContent = 1;
-    finalGrid[2].textContent = 2;
-    finalGrid[3].textContent = 3;
-    finalGrid[4].textContent = "";
-    finalGrid[5].textContent = 4;
-    finalGrid[6].textContent = 5;
-    finalGrid[7].textContent = 6;
-    finalGrid[8].textContent = "";
-    finalGrid[9].textContent = 7;
-    finalGrid[10].textContent = 8;
-    finalGrid[11].textContent = 9;
-    finalGrid[12].textContent = "";
+    finalGrid[0].textContent = 1;
+    finalGrid[1].textContent = 2;
+    finalGrid[2].textContent = 3;
+    finalGrid[3].textContent = "";
+    finalGrid[4].textContent = 4;
+    finalGrid[5].textContent = 5;
+    finalGrid[6].textContent = 6;
+    finalGrid[7].textContent = "";
+    finalGrid[8].textContent = 7;
+    finalGrid[9].textContent = 8;
+    finalGrid[10].textContent = 9;
+    finalGrid[11].textContent = "";
     // add divs to right three buttons
     //adding two divs to add/subtract button to split them up
     //subtract button
     const addButton = document.createElement("div");
     addButton.id = "addButton";
-    finalGrid[4].appendChild(addButton);
+    finalGrid[3].appendChild(addButton);
     addButton.textContent = "+"
     //add button
     const subtractButton = document.createElement("div");
     subtractButton.id = "subtractButton";
-    finalGrid[4].appendChild(subtractButton);
+    finalGrid[3].appendChild(subtractButton);
     subtractButton.textContent = "-"
     
     //adding two divs to multiply/divide button to split them up
     //multiply button
     const multiplyButton = document.createElement("div");
     multiplyButton.id = "multiplyButton";
-    finalGrid[8].appendChild(multiplyButton);
+    finalGrid[7].appendChild(multiplyButton);
     multiplyButton.textContent = "*"
     //divide button
     const divideButton = document.createElement("div");
     divideButton.id = "divideButton";
-    finalGrid[8].appendChild(divideButton);
+    finalGrid[7].appendChild(divideButton);
     divideButton.textContent = "/"
 
      //adding two divs to equal/clear button to split them up
      //equal button
      const equalButton = document.createElement("div");
      equalButton.id = "equalButton";
-    finalGrid[12].appendChild(equalButton);
+    finalGrid[11].appendChild(equalButton);
     equalButton.textContent = "="
 
     //clear button
     const clearButton = document.createElement("div");
     clearButton.id = "clearButton";
-    finalGrid[12].appendChild(clearButton);
+    finalGrid[11].appendChild(clearButton);
     clearButton.textContent = "Clear"
+
+
+    //functions that populate display when digit buttons are clicked
+    // make it a loop?
+    console.log(finalGrid)
+    for (let a = 0; a < finalGrid.length; a++) {
+        finalGrid[a].addEventListener("click", () => {
+        screenDiv.textContent = finalGrid[a].textContent;
+        });
+    }
+
 }
+
+
 
 //Created my parent div "container" in the html file
 const container = document.querySelector("#container");
@@ -112,3 +126,4 @@ const gridParent = document.createElement("div");
 container.appendChild(gridParent);
 gridParent.id = "gridParent"
 buildGrid(12);
+
