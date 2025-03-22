260bd0fdb5572b63a45a753f0e13ff4d53e27eb4
jackp50 <jpearne9@gmail.com>
2025-03-22 13:23:52 -0600

    Added the four basic math functions, then one operate function which finds out what operator the user used and then calls one of the four basic functions

---
+++ script.js
@@ -0,0 +1,41 @@
+let num1 = null;
+let num2 = null;
+let operator = "";
+
+function add(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+function subtract(num1, num2) {
+    let result = num1 - num2;
+    return result;
+}
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+function divide(num1, num2) {
+    let result = num1 / num2;
+    return result;
+}
+
+function operate(num1, operator, num2) {
+    if (operator == "+") {
+        return add(num1, num2);
+    }
+    else if (operator == "-") {
+        return subtract(num1, num2);
+    }
+    else if (operator == "*") {
+        return multiply(num1, num2);
+    }
+    else if (operator == "/") {
+        return divide(num1,num2);
+    }
+    else {
+        return "Please enter in +,-,*, or /"
+    }
+}
